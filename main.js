7c6a50fe6db418eedd4e94ce1e4ac15e96834ade
Fridtjof Høvk <fridtjofgh@gmail.com>
2016-06-27 05:52:03 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+console.log("JS file is connected to HTML! Woo!")
+s
+
+var cardOne = Queen;
+var cardTwo = Queen;
+var cardThree = King;
+var cardFour = King; 
+/*
+if (king === king || Queen===Queen) {
+	alert = "You found a match!"
+}
+else if ( king === Queen || Queen === king) {
+	alert = "Sorry, try again! "
+}
+*/
+ 
+function createBoard () {
+	var divs = document.getElementById('game-board');
+	var cards = 4;
+
+for(var i = 0; i < cards; i++){ 
+       var myDiv = document.createElement("div");
+       myDiv.className = "card";
+       boardDiv.appendChild(myDiv);
+} 
+
+var cards = [queen, queen, king, king];
+var cardsInPlay = function [];
+
+var createBoard = function () {
+
+}
+
+
+
